c8aed2ee663918635d405e69cc41d660c94ff2e0
3kyro <k.papachrisanthou@gmail.com>
2020-06-12 08:56:32 +0200

    clean up

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
+{-|
+Module : Main
+Description: A Modbus TCP CLI and web server
+
+Modbus-serve is a Modbus TCP command line communication tool and web server.
+-}
 module Main where
 
-import Lib
 
 main :: IO ()
-main = someFunc
+main = return ()
