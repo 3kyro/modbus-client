011dd2a93139160ae49dc505891244fde216f870
3kyro <k.papachrisanthou@gmail.com>
2020-07-17 10:02:08 +0200

    comments

--- mbserver/src/main.rs
+++ mbserver/src/main.rs
@@ -13,22 +13,27 @@ fn main() {
     // Since the server will only be used within a testing enviroment, simply panic on each fault
     let filename = args().nth(1).expect("mbserver: no register file given");
 
+    // read thje template file
     let mut file = File::open(filename).expect("mbserver: error opening register file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("mbserver: error reading register file");
     std::mem::drop(file);
 
+    // parse registers and create the server
     let registers = parse_registers(&contents);
     let mbserver = create_server(registers);
 
+    // the test server is always running on localhost:5502 to avoid the need to be
+    // run as super user
     let socket_addr = "127.0.0.1:5502".parse().unwrap();
 
-    let sv = Arc::new(Mutex::new(mbserver));
+    let server = Arc::new(Mutex::new(mbserver));
 
-    let _server = thread::spawn(move || {
+    // spawn the serever in a dedicated thread
+    thread::spawn(move || {
         server::tcp::Server::new(socket_addr)
-            .serve(move || Ok(Arc::clone(&sv).lock().unwrap().clone()));
+            .serve(move || Ok(Arc::clone(&server).lock().unwrap().clone()));
     });
     // Give the server some time for stating up
     thread::sleep(Duration::from_secs(1));
