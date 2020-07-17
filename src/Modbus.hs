bdeffa59365f5023d4c03a48839bce5972b7b1ab
3kyro <k.papachrisanthou@gmail.com>
2020-07-17 19:50:07 +0200

    cleanup

--- src/Modbus.hs
+++ src/Modbus.hs
@@ -1,16 +1,13 @@
 module Modbus (modSession) where
 
-import Control.Monad.Except (runExceptT, throwError)
+import Control.Monad.Except (throwError)
 import Data.Maybe (listToMaybe)
 import Data.Word (Word16)
-import Network.Socket.ByteString (recv, send)
 import Data.Binary.Put (runPut, putWord16le)
 import Data.Binary.Get (runGet, getFloatbe, getFloatle)
 
-import CsvParser (ModData (..), ModType (..), RegType (..), ByteOrder (..), modData)
+import CsvParser (ModData (..), ModType (..), RegType (..), ByteOrder (..))
 
-import qualified Data.Text as T
-import qualified Network.Socket as S
 import qualified System.Modbus.TCP as MB
 
 modSession :: [ModData] -> ByteOrder -> MB.Session [ModData]
@@ -23,7 +20,6 @@ modSession md order =
 genSession :: (Word16, ModData) -> ByteOrder -> MB.Session ModData
 genSession (idx, md) order =
   let 
-    addr = MB.unRegAddress $ register md
     tp = regType md
   in 
     case tp of
@@ -74,55 +70,11 @@ getFloat order ws
     | order == BE = Just $ be2float ws 
 
 le2float :: (Word16, Word16) -> Float
-le2float (fst, snd)= runGet getFloatle $ runPut putWords 
+le2float (f, s)= runGet getFloatle $ runPut putWords 
   where
-    putWords = putWord16le fst >> putWord16le snd
+    putWords = putWord16le f >> putWord16le s
 
 be2float :: (Word16, Word16) -> Float
-be2float (fst, snd)= runGet getFloatbe $ runPut putWords 
+be2float (f, s)= runGet getFloatbe $ runPut putWords 
   where
-    putWords = putWord16le fst >> putWord16le snd
-
----------------------------------------------------
---- Explorarory stuff, nothing to see here!
----------------------------------------------------
-
-testData :: [ModData]
-testData =
-  [ modData (T.pack "foo") InputRegister 1 (ModWord Nothing) (T.pack ""),
-    modData (T.pack "bar") InputRegister 0 (ModWord Nothing) (T.pack ""),
-    modData (T.pack "faz") InputRegister 2 (ModWord Nothing) (T.pack ""),
-    modData (T.pack "pee") InputRegister 3 (ModWord Nothing) (T.pack "")
-  ]
-
-getStatus :: MB.Session [Word16]
-getStatus = MB.readInputRegisters 0 0 0 0 5
-
-getPower :: MB.Session [Word16]
-getPower = MB.readInputRegisters 0 0 0 0 6
-
-combined :: MB.Session [[Word16]]
-combined = do
-  a <- getStatus
-  b <- getPower
-  return [a, b]
-
-local :: S.Socket -> MB.Connection
-local s =
-  MB.Connection
-    { MB.connWrite = send s,
-      MB.connRead = recv s,
-      MB.connCommandTimeout = 1000,
-      MB.connRetryWhen = \e n -> False
-    }
-
-addr = S.SockAddrInet 5502 (S.tupleToHostAddress (127, 0, 0, 1))
-
-try :: S.SockAddr -> IO ()
-try address = do
-  putStrLn ("Connecting to " ++ show address ++ "...")
-  s <- S.socket S.AF_INET S.Stream S.defaultProtocol
-  S.connect s address
-  putStrLn "connected"
-  a <- runExceptT $ MB.runSession (local s) (modSession testData LE)
-  print a
+    putWords = putWord16le f >> putWord16le s
