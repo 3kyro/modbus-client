e917bba9932596f9e77f0c0d1edcfef0e17ffc14
3kyro <k.papachrisanthou@gmail.com>
2020-08-06 15:52:25 +0200

    add write ModData

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,14 +1,26 @@
 module Repl (runRepl) where
 
 import Control.Monad.Trans (liftIO)
-import Control.Monad.Reader (ReaderT, runReaderT, ask)
-import Control.Monad.Trans.Except (runExceptT, ExceptT, except, mapExceptT)
+import Control.Monad.Reader (zipWithM, ReaderT, runReaderT, ask)
+import Control.Monad.Trans.Except 
+    (
+      runExceptT
+    , ExceptT
+    , except
+    , mapExceptT
+    )
 import Control.Monad.IO.Class ()
 import Data.List (isPrefixOf, uncons, find)
 import Data.Maybe (fromJust, listToMaybe)
 import Data.Word (Word16)
 import Data.Either.Combinators (mapLeft)
-import System.Console.Repline (HaskelineT, evalRepl, WordCompleter, CompleterStyle( Word ))
+import System.Console.Repline 
+    (
+       HaskelineT
+     , evalRepl
+     , WordCompleter
+     , CompleterStyle( Word )
+    )
 import Text.Parsec (ParseError, parse, eof, option, string)
 import Text.Parsec.Token (float, decimal, makeTokenParser)
 import Text.Parsec.Language (haskellDef)
@@ -61,6 +73,7 @@ cmd input =
         "writeSingleRegisterFloat" -> writeSingleRegisterFloat args
         "writeMultipleRegistersFloat" -> writeMultipleRegistersFloat args
         "read" -> readModData args
+        "write" -> writeModData args
         _ -> liftIO $ putStrLn ("command not found: " ++ command)
 
 -- Tab Completion: return a completion for partial words entered
@@ -93,50 +106,118 @@ commands = [
     , "writeSingleRegisterFloat"
     , "writeMultipleRegistersFloat"
     , "read"
+    , "write"
     ]
 
 readRegistersWord :: ReadRegsFun -> [String] -> Repl ()
 readRegistersWord f [address, number] = do
     response <- replReadRegisters address number (ModWord Nothing) f
     liftIO $ print response
-readRegistersWord _ _ = liftIO $ putStrLn "Invalid command arguments"
+readRegistersWord _ _ = invalidCmd
 
 readRegistersFloat :: ReadRegsFun -> [String] -> Repl ()
 readRegistersFloat f [address, number] = do
     Config _ order _ <- ask
     response <- replReadRegisters address number (ModFloat Nothing) f
     liftIO $ print (getFloats order response)
-readRegistersFloat _ _ = liftIO $ putStrLn "Invalid command arguments"
+readRegistersFloat _ _ = invalidCmd
 
 writeSingleRegisterWord :: [String] -> Repl ()
 writeSingleRegisterWord [address, regValue] = 
     replWriteRegisters address [regValue] (ModWord Nothing)
-writeSingleRegisterWord _ = liftIO $ print "Invalid command arguments"
+writeSingleRegisterWord _ = invalidCmd
 
 writeMultipleRegistersWord :: [String] -> Repl ()
 writeMultipleRegistersWord (address:values) = 
     replWriteRegisters address values (ModWord Nothing) 
-writeMultipleRegistersWord _ = liftIO $ print "Invalid command arguments"
+writeMultipleRegistersWord _ = invalidCmd
 
 writeSingleRegisterFloat :: [String] -> Repl ()
 writeSingleRegisterFloat [address, regValue] = 
     replWriteRegisters address [regValue] (ModFloat Nothing) 
-writeSingleRegisterFloat _ = liftIO $ print "Invalid command arguments"
+writeSingleRegisterFloat _ = invalidCmd
 
 writeMultipleRegistersFloat :: [String] -> Repl ()
 writeMultipleRegistersFloat (address:values) = 
     replWriteRegisters address values (ModFloat Nothing) 
-writeMultipleRegistersFloat _ = liftIO $ print "Invalid command arguments"
+writeMultipleRegistersFloat _ = invalidCmd
 
 readModData :: [String] -> Repl ()
-readModData [] = liftIO $ print "Invalid command arguments"
+readModData [] = invalidCmd
 readModData args = do 
     Config _ _ mdata <- ask
     let wrapped = except $ getModByDescription args mdata
     mds <- replRunExceptT wrapped (const [])
     response <- mapM readMod mds
     liftIO $ mapM_ print response
+
+writeModData :: [String] -> Repl ()
+writeModData [] = invalidCmd
+writeModData args = do
+    Config _ _ mdata <- ask
+    let mPairs = getPairs args
+    case mPairs of
+        Nothing -> invalidCmd
+        Just pairs -> do
+            let wrapped = except $ getModByPair pairs mdata
+            mds <- replRunExceptT wrapped  (const [])
+            written <- mapM writeMod mds  
+            liftIO $ putStrLn $ show (sum written) ++ " value(s) written" 
+
+-- Returns a pair of successive values
+-- Returns Nothing if the list size is odd
+getPairs :: [a] -> Maybe ([a], [a])
+getPairs [] = Just ([],[])
+getPairs [_] = Nothing
+getPairs (x:y:zs) = (,) <$> firsts <*> seconds
+  where 
+    rest = getPairs zs
+    firsts =  (:) <$> Just x <*> (fst <$> rest)
+    seconds = (:) <$> Just y <*> (snd <$> rest)
+
+-- Writes a ModData to the server
+-- Returns the number of ModData written
+writeMod :: ModData -> Repl Word16
+writeMod md = do
+    let address = modAddress md
+    let modVal = modValue md
+    let regType = modRegType md
+    case regType of
+        HoldingRegister -> do
+            case modVal of
+                ModWord _ -> writeModWord modVal address
+                ModFloat _ -> writeModFloat modVal address
+        _ -> do
+            liftIO $ 
+                putStrLn $  "Invalid register type for value: " 
+                            ++ modName md
+            return 0 
+
+-- Write a ModWord to the modbus server.
+-- Returns the number of ModWords written
+writeModWord :: ModType -> Word16 -> Repl Word16
+writeModWord (ModWord (Just word)) address = do
+    Config connection _ _ <- ask
+    let addr = MB.RegAddress address
+    let writeSession = runReplSession connection $ MB.writeMultipleRegisters 0 0 255 addr [word]
+    replRunExceptT writeSession (const 0)  
+writeModWord (ModWord _) _ = return 0
+
+-- Write a ModFloat to the modbus server.
+-- Returns the number of ModFloats written
+writeModFloat :: ModType -> Word16 -> Repl Word16
+writeModFloat (ModFloat (Just fl)) address = do
+    Config connection _ _ <- ask
+    let addr = MB.RegAddress address
+    let ordWords = fromFloats [fl]
+    let writeSession = runReplSession connection $ MB.writeMultipleRegisters 0 0 255 addr ordWords
+    wordsWritten <- replRunExceptT writeSession (const 0)
+    return $ wordsWritten `div` 2
     
+-- Checks if a list of descriptions are all valid when
+-- checked against a list of ModData    
+-- All descriptions must exist in the provided list
+-- If a description does not exist, a ReplError is returned
 getModByDescription :: [String] -> [ModData] -> Either ReplError [ModData]
 getModByDescription [] _ = Right []
 getModByDescription (x:xs) mdata = do
@@ -146,6 +227,25 @@ getModByDescription (x:xs) mdata = do
         Nothing -> Left $ ReplCommandError $ "Modbus Register " ++ x ++ " not found on template file." 
         Just hit -> (:) <$> return hit <*> getModByDescription xs mdata
 
+-- Generate a list of Moddata from pairs of Description and values
+getModByPair :: ([String], [String]) -> [ModData] -> Either ReplError [ModData]
+getModByPair ([], _) _ = Right []
+getModByPair (descs, vals) mds = do
+    validMd <- getModByDescription descs mds
+    zipWithM insertValue validMd vals
+
+-- Parses and inserts a value to a ModData
+insertValue :: ModData -> String -> Either ReplError ModData
+insertValue md val = do
+    case modValue md of
+        ModWord _ -> do
+            parsedV <- pReplWord val
+            return $ md {modValue = ModWord (Just parsedV)}
+        ModFloat _ -> do
+            parsedV <- pReplFloat val
+            return $ md {modValue = ModFloat (Just parsedV)}
+
+-- Reads a ModData from the server
 readMod :: ModData -> Repl ModData
 readMod md = do
     Config connection order _ <- ask
@@ -176,7 +276,7 @@ replReadRegisters a n m f = do
     Config connection _ _ <- ask
     let mult = getModTypeMult m
     let wrapped = do 
-            (addr, num) <- except $ pAddressNumber a n 
+            (addr, num) <- except $ pReplAddrNum a n 
             liftIO $ putStrLn $ "Reading " ++ show num ++ " register(s) from address " ++ show addr
             runReplSession connection $ f 0 0 255 (MB.RegAddress addr) (mult * num)
     replRunExceptT wrapped (const [])
@@ -191,10 +291,10 @@ replWriteRegisters ::
 replWriteRegisters address values modtype = do
     Config connection _ _ <- ask
     let wrapped = do
-            addr <- except $ pWord address
+            addr <- except $ pReplWord address
             val <- case modtype of
-                ModWord _ -> except $ mapM pWord values
-                ModFloat _ -> except $ fromFloats <$> mapM pFloat values
+                ModWord _ -> except $ mapM pReplWord values
+                ModFloat _ -> except $ fromFloats <$> mapM pReplFloat values
             liftIO $ putStrLn $ "Writing " ++ regsWritten val ++ " register(s) at address " ++ show addr
             runReplSession connection $ MB.writeMultipleRegisters 0 0 255 (MB.RegAddress addr) val
     response <- replRunExceptT wrapped (const 0) 
@@ -221,19 +321,22 @@ replRunExceptT ex f = do
         Right x -> return x 
 
 -- Parse address and number of register strings 
-pAddressNumber :: String -> String-> Either ReplError (Word16, Word16)
-pAddressNumber a n = (,) <$> pWord a <*> pWord n
+pReplAddrNum :: String -> String-> Either ReplError (Word16, Word16)
+pReplAddrNum a n = (,) <$> pReplWord a <*> pReplWord n
 
-pFloat :: String -> Either ReplError Float
-pFloat s = mapLeft ReplParseError parseResult
+invalidCmd :: Repl ()
+invalidCmd = liftIO $ putStrLn "Invalid Command argument"
+
+pReplFloat :: String -> Either ReplError Float
+pReplFloat s = mapLeft ReplParseError parseResult
   where 
     parseResult = parse (pRawFloat <* eof) "" s
     pRawFloat = read <$> ((++) <$> sign <*> pf)
     sign = option "" (string "-")
     pf = show <$> float (makeTokenParser haskellDef)   
         
-pWord :: String -> Either ReplError Word16
-pWord s = mapLeft ReplParseError parseResult  
+pReplWord :: String -> Either ReplError Word16
+pReplWord s = mapLeft ReplParseError parseResult  
   where
     parseResult = parse (pRawWord <* eof) "" s
     pRawWord = fromInteger <$> decimal (makeTokenParser haskellDef)
